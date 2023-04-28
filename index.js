48ba83200063325b996972043a8c25a4c9867c27
archi-potapov <arturpotapov98@gmail.com>
2023-04-28 22:38:17 +0300

    v.0.0.1

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const qs = require('querystring');
+// const XMLHttpRequest = require('xhr2');
+const server = http.createServer((request, resolve) => {
+
+    request.on('data', (data) => {
+        console.log(qs.parse(data.toString()));
+    })
+
+    resolve.setHeader('Content-Type', 'application/json');
+
+    const newData = JSON.stringify({
+        "error": "",
+        "login": "sasi huy"
+    })
+    resolve.end(newData);
+});
+
+server.listen(3000, 'localhost', (error) => {
+    error ? console.log(error) : console.log('listening port 3000...');
+})
