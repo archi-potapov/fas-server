150cd5b4436af605c0d18ac1b73c9bd7e7b298cd
archi-potapov <arturpotapov98@gmail.com>
2023-05-09 16:20:31 +0300

    v.0.0.4

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-const http = require("http");
-const qs = require('querystring');
-// const XMLHttpRequest = require('xhr2');
-console.log("SER");
-const server = http.createServer((request, resolve) => {
+import express from "express";
+import bodyParser from "body-parser";
 
-    request.on('data', (data) => {
-        console.log(qs.parse(data.toString()));
-    })
+const PORT = 3001;
+const app = express();
 
-    resolve.setHeader('Content-Type', 'application/json');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-    const newData = JSON.stringify({
-        "error": "",
-        "login": "sasi huy"
-    })
-    resolve.end(newData);
+app.get("/", (req, res) => {
+    console.log("MARAKUYA");
+})
+app.post('/',(req, res) => {
+    let body = '';
+    // let json = req.json();
+    console.log(req.body);
+    req.on('data', chunk => {
+        body += chunk.toString();
+        console.log(body);
+    });
+    req.on('end', () => {
+        console.log(JSON.parse(body));
+    });
+    res.send({"login": "logggin", "error": ""});
+})
+
+app.listen(PORT, () => {
+    console.log(`The server has been started on port ${PORT}...`);
 });
 
-server.listen(3001, 'localhost', (error) => {
-    error ? console.log(error) : console.log('listening port 3001...');
-})
+// const http = require("http");
+// const qs = require('querystring');
+// // const XMLHttpRequest = require('xhr2');
+// console.log("SER");
+// const server = http.createServer((request, resolve) => {
+//
+//     request.on('data', (data) => {
+//         console.log(qs.parse(data.toString()));
+//     })
+//
+//     resolve.setHeader('Content-Type', 'application/json');
+//
+//     const newData = JSON.stringify({
+//         "error": "",
+//         "login": "sasi huy"
+//     })
+//     resolve.end(newData);
+// });
+//
+// server.listen(3001, 'localhost', (error) => {
+//     error ? console.log(error) : console.log('listening port 3001...');
+// })
