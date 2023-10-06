57ec5d943338d04ef62fc09007c48e48d2ed0346
archi-potapov <arturpotapov98@gmail.com>
2023-10-06 23:59:10 +0300

    fix

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
-import https from "https";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 
 const PORT = 3001;
 const app = express();
-
-// const sslServer = https.createServer({
-//     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-// }, app)
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
