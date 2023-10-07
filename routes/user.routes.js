b8788b274ba0f271cd6a8d70d07657187868f488
archi-potapov <arturpotapov98@gmail.com>
2023-10-07 04:30:02 +0300

    fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import userController from "../controller/user.controller";
 const router = new Router();
 
-router.post("", userController.createUser)
-router.get("", userController.getUsers)
+router.post((req, res) => {
+  res.send(req.body);
+});
+router.get("", (req, res) => {
+  res.send(req.body);
+});
 
-module.exports = router
+module.exports = router;
