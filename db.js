9a9197306f5480ea9464f9467a79621168e5d8b3
archi-potapov <arturpotapov98@gmail.com>
2023-10-07 05:11:26 +0300

    fix

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
     user: "postgres",
-    password: "aezakmi1029345",
+    password: "testdbpass",
     host: "localhost",
     port: 5432,
     database: "users"
