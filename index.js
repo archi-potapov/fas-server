e63a6f971a0301a39f1e8b8cb36a9f869acac193
archi-potapov <arturpotapov98@gmail.com>
2023-10-07 15:36:11 +0300

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (request, resolve) => {
+app.get("/auth", (request, resolve) => {
   //   console.log(request.hostname);
   resolve.send("Success");
 });
